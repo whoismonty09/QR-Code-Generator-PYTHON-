41f96cdf0d64f375be20ca12edcd17b725194a8a
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-14 09:51:32 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import qrcode
+print("QR Code Generator developed by Monty")
+data = input("Enter text or URL: ")
 
+qr = qrcode.make(data)
+
+qr.save("qrcode.png")
+
+print("QR code generated successfully")
